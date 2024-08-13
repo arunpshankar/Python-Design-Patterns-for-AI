27b6ad7cb41f67d19195211be1fcc6a337909706
Arun Shankar <arunprshankar@gmail.com>
2024-08-13 12:56:39 -0500

    wip

--- src/patterns/01_singleton/example_01.py
+++ src/patterns/01_singleton/example_01.py
@@ -1,7 +1,8 @@
 from src.config.logging import logger 
 
 class ModelConfig:
-    _instance = None  # Class-level variable to hold the single instance of the class
+    # Class-level variable to hold the single instance of the class
+    _instance = None  
 
     def __new__(cls, *args, **kwargs):
         """
@@ -13,10 +14,10 @@ class ModelConfig:
             cls._instance = super(ModelConfig, cls).__new__(cls, *args, **kwargs)
             cls.model_name = "Gemini 2.0"
             cls.model_path = "/models/gemini"
-            cls.api_key = "xyz-abc"
+            cls.api_key = "xyz-abc-123"
         return cls._instance
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config1 = ModelConfig()
     logger.info(config1)
 
